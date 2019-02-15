43fa86567b57088226f6b72f08d70225f5fc12be
Chexmate45 <45887882+Chexmate45@users.noreply.github.com>
2019-02-14 18:55:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import sys
 
 if len(sys.argv) != 2:
-        sys.exit("Argument usage: python main.py (txt directory) (password to c$
+	sys.exit("Argument usage: python main.py (password you want to check)")
 
 passList = open("list.txt", "r")
 for line in passList:
-        if (line.strip() == str(sys.argv[2])):
-                print("Password found: " + line)
-                break
-        print(line)
+	if (line.strip() == str(sys.argv[1])):
+		print("Password found: " + line)
+		break
+	print(line)
+
