5e16d3ea0fa2f1699549e86d61cb73a59ea0a71e
Chexmate45 <45887882+Chexmate45@users.noreply.github.com>
2019-02-14 18:49:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import os
 import sys
 
-if len(sys.argv) != 4:
-	sys.exit("Argument usage: python main.py (txt directory) (password to crack) (name of the word list file not including .txt")
+if len(sys.argv) != 2:
+        sys.exit("Argument usage: python main.py (txt directory) (password to c$
 
-for root,dir, files in os.walk(sys.argv[1]):
-	for file in files:
-		if (file.endswith('.txt') and file.startswith(sys.argv[3])):
-			file = os.path.join(root, file)
-
-
-passList = open(file, "r")
+passList = open("list.txt", "r")
 for line in passList:
-	if (line.strip() == str(sys.argv[2])):
-		print("Password found: " + line)
-		break
-	print(line)
-
+        if (line.strip() == str(sys.argv[2])):
+                print("Password found: " + line)
+                break
+        print(line)
